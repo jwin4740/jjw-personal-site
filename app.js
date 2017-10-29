798b4c4a379cf39dbeb57762ada755f91f028f32
jwin4740 <jwin4740@gmail.com>
2017-10-29 15:20:42 -0400

    serve it

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
+#!/usr/bin/env nodejs
+
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
-var index = require('./routes/index');
+
 const port = 8080;
 
 var app = express();
 
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-app.get('/', function(req, res, next) {
-  res.send('index.html');
+app.get('/', function (req, res, next) {
+    res.send('index.html');
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 
 
 app.listen(port, function () {
-  console.log('server listening on port:', port)
-})
+    console.log('server listening on port:', port)
+});
 
