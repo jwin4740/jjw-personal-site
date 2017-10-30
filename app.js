f3f430a6fb905e7898b3f541baecbd3caf20d58f
jwin4740 <jwin4740@gmail.com>
2017-10-29 22:49:30 -0400

    aboutme and tech set

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
     res.send('index.html');
 });
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function (req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 
 app.listen(port, function () {
